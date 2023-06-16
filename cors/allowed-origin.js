fbe14db3598c5b7d95baf9f30a3e92b48f4075c3
perdanaph <danangputro9@gmail.com>
2023-06-16 19:59:39 +0700

    refactoring: fixing cors

--- cors/allowed-origin.js
+++ cors/allowed-origin.js
@@ -1,9 +1,2 @@
-const allowOrigin=[
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-    "http://127.0.0.1:5172",
-    "http://localhost:5172",
-    "http://localhost",
-    "https://calc-health.vercel.app/"
-]
-module.exports= allowOrigin
+const allowOrigin = ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:5172', 'http://localhost:5172', 'http://localhost', 'https://calc-health.vercel.app'];
+module.exports = allowOrigin;
