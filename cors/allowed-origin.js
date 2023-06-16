942504f081a9ca1118db198ec3f6332f5fe71dbe
Fakhrezi Fajris <fajrisfakhrezi@gmail.com>
2023-06-16 19:45:16 +0700

    deploying

--- cors/allowed-origin.js
+++ cors/allowed-origin.js
@@ -3,6 +3,7 @@ const allowOrigin=[
     "http://localhost:5173",
     "http://127.0.0.1:5172",
     "http://localhost:5172",
-    "http://localhost"
+    "http://localhost",
+    "https://calc-health.vercel.app/"
 ]
 module.exports= allowOrigin
